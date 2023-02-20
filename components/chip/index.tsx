9b69d785f5810cc74f6c394f6c193f18cfe09db8
Samson Nwokike <nwokikesamson@gmail.com>
2023-02-20 10:02:05 +0100

    added more components and graphql queries

---
+++ components/chip/index.tsx
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+interface ChipProps {
+  id: string;
+  text: string;
+  isClickable: boolean;
+  onClick?: (id: string) => void;
+}
+
+const Chip: React.FC<ChipProps> = ({ text, onClick, isClickable, id }) => {
+  return (
+    <Container isClickable={isClickable} onClick={() => onClick && onClick(id)}>
+      {text}
+    </Container>
+  );
+};
+
+const Container = styled.span<{ isClickable: boolean }>`
+  margin-right: 10px;
+  margin-bottom: 6px;
+  border: 1px solid white;
+  border-radius: 4px;
+  padding: 6px;
+  display: inline-block;
+  font-size: 14px;
+  transition: all 0.3s;
+  &:hover {
+    background-color: black;
+    cursor: ${(props) => (props.isClickable ? 'pointer' : 'default')};
+  }
+`;
+
+export default Chip;
